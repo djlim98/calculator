9e92f4ea98db42d7da5d6f3ad7c69e9acde91591
Dongjin Lim <42141895+djlim98@users.noreply.github.com>
2021-03-19 01:14:53 +0900

    Update people.controller.js

--- src/controllers/people.controller.js
+++ src/controllers/people.controller.js
@@ -2,21 +2,27 @@ const People=require('../models/people.model');
 const Calendar=require('../models/calendar.model');
 
 exports.calculate=(req,res)=>{
-    const people=new People(req.body.people);
-    const {year, month,holiday, extraTime}=req.body;
-    console.log(year);
+    const {people,year, month,holiday, extraTime}=req.body;
+    const peopleObj=new People(people,extraTime);
     const calendar=new Calendar(year,month,holiday);
     
-    console.log(calendar);
+    console.log(peopleObj);
 
     calendar.date.forEach(date => {
         const timeList=['morning','middle','night1','night2'];
+        const dayWorker=[];
         timeList.forEach(time => {
-            people.add_time(time,date);    
+            const worker=peopleObj.add_time(time,date, dayWorker);
+            dayWorker.push(worker);
+            calendar.workSchedule[date][time]=worker;
         });
+        console.log(dayWorker);
         
     });
-    console.log(people);
-    res.status(200).send(people)
-    return
+    console.log(calendar);
+    peopleObj.people.sort(function(a, b) { 
+        return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+    });
+    console.log(peopleObj.people);
+    res.status(200).send({people:peopleObj.people, workSchedule:calendar.workSchedule});
 };
